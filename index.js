742df4edc88ba9569f3bb152beb7819a557638bf
Arpita Gupta <59776939+ArpitaGupta4086@users.noreply.github.com>
2021-08-06 12:08:34 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function updateMap() {
+    console.log("Updating map with realtime data")
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(rsp => {
+            // console.log(rsp.data)
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+
+                cases = element.infected;
+                if (cases>255){
+                    color = "rgb(0,0,255)";
+                }
+                else{
+                    color = `rgb(0,0,${cases})`;
+                }
+                //Mark on the map
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+            });
+        })
+}
+updateMap();
+ let interval = 1000;
+ setInterval(updateMap,interval);
